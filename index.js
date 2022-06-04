7bffa822ec0c30456ce9787ee34b3f50e9fd5931
tayeful912 <kazitayefulislam12@gmail.com>
2022-06-04 17:38:11 +0600

    initialize the server project

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express()
+const port = process.env.PORT || 5000;
+
+// middle ware
+
+app.use(cors())
+app.use(express.json())
+
+
+app.get('/', (req, res) => {
+    res.send('Server is runtting ar port')
+})
+
+app.listen(port, () => {
+    console.log('server is running ar port', port)
+})
+
