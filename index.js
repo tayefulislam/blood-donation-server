b98df1e99584573faf09b2302356ef83b314d605
tayeful912 <kazitayefulislam12@gmail.com>
2022-06-05 01:11:53 +0600

    recive blood request api crated

--- index.js
+++ index.js
@@ -1,13 +1,63 @@
 const express = require("express");
 const cors = require("cors");
+require('dotenv').config();
 
 const app = express()
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5000
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 // middle ware
 
 app.use(cors())
 app.use(express.json())
+
+
+// connect to database 
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.69der.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri)
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+// client.connect(err => {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   client.close();
+// });
+
+
+
+async function run() {
+
+    try {
+
+        await client.connect()
+
+        const requestCollection = client.db("bloodDonation1").collection("bloodRequests");
+
+
+        app.post('/bloodRequest', async (req, res) => {
+            const newRequest = req.body;
+            const result = await requestCollection.insertOne(newRequest);
+            res.send(result)
+        })
+
+
+
+
+
+
+    }
+
+    finally {
+
+    }
+
+}
+
+run().catch(console.dir)
 
 
 app.get('/', (req, res) => {
