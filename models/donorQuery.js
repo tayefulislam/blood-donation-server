ec02d9f518f0beedb0f33c420b9bfc7040c5389c
tayefulislam <kazitayefulislam12@gmail.com>
2024-03-31 04:07:41 +0600

    timestapme added

--- models/donorQuery.js
+++ models/donorQuery.js
@@ -1,23 +1,28 @@
 const mongoose = require("mongoose");
 
-const DonorQuerySchema = mongoose.Schema({
-  donorQueryByDistrictAndGroup: {
-    type: String,
-    trim: true,
+const DonorQuerySchema = mongoose.Schema(
+  {
+    donorQueryByDistrictAndGroup: {
+      type: String,
+      trim: true,
+    },
+    district: {
+      type: String,
+      trim: true,
+    },
+    group: {
+      type: String,
+      trim: true,
+    },
+    count: {
+      type: Number,
+      default: 1,
+    },
   },
-  district: {
-    type: String,
-    trim: true,
-  },
-  group: {
-    type: String,
-    trim: true,
-  },
-  count: {
-    type: Number,
-    default: 1,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 const DonorQuery = mongoose.model("DonorQuery", DonorQuerySchema, "DonorQuery");
 module.exports = DonorQuery;
--- models/publicDonors.js
+++ models/publicDonors.js
@@ -1,50 +1,55 @@
 const mongoose = require("mongoose");
 
-const PublicDonorsSchema = mongoose.Schema({
-  number: {
-    type: String,
-    required: true,
-    unique: true,
-    index: true,
-  },
+const PublicDonorsSchema = mongoose.Schema(
+  {
+    number: {
+      type: String,
+      required: true,
+      unique: true,
+      index: true,
+    },
 
-  name: {
-    type: String,
-    trim: true,
-    required: true,
-  },
-  group: {
-    type: String,
-    trim: true,
-  },
+    name: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+    group: {
+      type: String,
+      trim: true,
+    },
 
-  district: {
-    type: String,
-    trim: true,
-  },
-  area: {
-    type: String,
-    trim: true,
-  },
-  role: {
-    type: String,
-    enum: ["admin", "user"],
-    default: "user",
-  },
-  lastDonation: {
-    type: String,
-  },
+    district: {
+      type: String,
+      trim: true,
+    },
+    area: {
+      type: String,
+      trim: true,
+    },
+    role: {
+      type: String,
+      enum: ["admin", "user"],
+      default: "user",
+    },
+    lastDonation: {
+      type: String,
+    },
 
-  status: {
-    type: String,
-    enum: ["active", "inactive"],
-    default: "active",
+    status: {
+      type: String,
+      enum: ["active", "inactive"],
+      default: "active",
+    },
+    gender: {
+      type: String,
+      trim: true,
+    },
   },
-  gender: {
-    type: String,
-    trim: true,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 const PublicDonors = mongoose.model(
   "PublicDonors",
